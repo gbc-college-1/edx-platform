5636fff87e0196364a51d7fca1d380214f0302c9
Irtaza Akram <irtaza.akram@arbisoft.com>
2025-12-15 16:45:26 +0500

    fix: pylint issues for problem xblock

--- xmodule/capa/tests/test_xqueue_interface.py
+++ xmodule/capa/tests/test_xqueue_interface.py
@@ -40,7 +40,7 @@ class XQueueServiceTest(TestCase):
         assert self.service.interface.session.auth.password == "agarwal"
 
     @patch("xmodule.capa.xqueue_interface.use_edx_submissions_for_xqueue", return_value=True)
-    def test_construct_callback_with_flag_enabled(self, mock_flag):
+    def test_construct_callback_with_flag_enabled(self, mock_flag):  # pylint: disable=unused-argument
         """Test construct_callback when the waffle flag is enabled."""
         usage_id = self.block.scope_ids.usage_id
         course_id = str(usage_id.course_key)
@@ -56,7 +56,7 @@ class XQueueServiceTest(TestCase):
             assert self.service.construct_callback() == f"{custom_callback_url}/{callback_url}/score_update"
 
     @patch("xmodule.capa.xqueue_interface.use_edx_submissions_for_xqueue", return_value=False)
-    def test_construct_callback_with_flag_disabled(self, mock_flag):
+    def test_construct_callback_with_flag_disabled(self, mock_flag):  # pylint: disable=unused-argument
         """Test construct_callback when the waffle flag is disabled."""
         usage_id = self.block.scope_ids.usage_id
         callback_url = f"courses/{usage_id.context_key}/xqueue/user1/{usage_id}"
@@ -83,7 +83,7 @@ class XQueueServiceTest(TestCase):
 @pytest.mark.django_db
 @patch("xmodule.capa.xqueue_interface.use_edx_submissions_for_xqueue", return_value=True)
 @patch("xmodule.capa.xqueue_submission.XQueueInterfaceSubmission.send_to_submission")
-def test_send_to_queue_with_flag_enabled(mock_send_to_submission, mock_flag):
+def test_send_to_queue_with_flag_enabled(mock_send_to_submission, mock_flag):  # pylint: disable=unused-argument
     """Test send_to_queue when the waffle flag is enabled."""
     url = "http://example.com/xqueue"
     django_auth = {"username": "user", "password": "pass"}
@@ -107,7 +107,7 @@ def test_send_to_queue_with_flag_enabled(mock_send_to_submission, mock_flag):
     files_to_upload = None
 
     mock_send_to_submission.return_value = {"submission": "mock_submission"}
-    error, msg = xqueue_interface.send_to_queue(header, body, files_to_upload)
+    error, msg = xqueue_interface.send_to_queue(header, body, files_to_upload)  # pylint: disable=unused-variable
 
     mock_send_to_submission.assert_called_once_with(header, body, {})
 
@@ -115,7 +115,7 @@ def test_send_to_queue_with_flag_enabled(mock_send_to_submission, mock_flag):
 @pytest.mark.django_db
 @patch("xmodule.capa.xqueue_interface.use_edx_submissions_for_xqueue", return_value=False)
 @patch("xmodule.capa.xqueue_interface.XQueueInterface._http_post")
-def test_send_to_queue_with_flag_disabled(mock_http_post, mock_flag):
+def test_send_to_queue_with_flag_disabled(mock_http_post, mock_flag):  # pylint: disable=unused-argument
     """Test send_to_queue when the waffle flag is disabled."""
     url = "http://example.com/xqueue"
     django_auth = {"username": "user", "password": "pass"}
@@ -139,7 +139,7 @@ def test_send_to_queue_with_flag_disabled(mock_http_post, mock_flag):
     files_to_upload = None
 
     mock_http_post.return_value = (0, "Submission sent successfully")
-    error, msg = xqueue_interface.send_to_queue(header, body, files_to_upload)
+    error, msg = xqueue_interface.send_to_queue(header, body, files_to_upload)  # pylint: disable=unused-variable
 
     mock_http_post.assert_called_once_with(
         "http://example.com/xqueue/xqueue/submit/",
